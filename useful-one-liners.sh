3e25e5f6234262c764f21ee197354817822d4386
Andru Beldie <andru.beldie@gmail.com>
2013-03-18 14:45:23 +0200

    Create useful-one-liners.sh

---
+++ useful-one-liners.sh
@@ -0,0 +1,14 @@
+# programatically change password for an user
+# echo "username:new_password" | /usr/sbin/chpasswd
+
+# get top 10 subfolders in current folder based on size
+# du -m . --max-depth=1 | sort -n -r | head -n 10
+
+# gziped mysqldump saved as curent date-hour-minute.sql.gz
+# mysqldump -u username -p -B database_name --single-transaction | gzip > /path_to_backup_folder/`date +%F-%H-%M.sql.gz`
+
+# one liner to check/visualize magento's DB credentials
+#head -n `cat app/etc/local.xml | grep -n "</connection>" | awk -F : '{print $1;}'` app/etc/local.xml | tail -n 11
+
+# load CSV or any other file into a table in MySQL
+# LOAD DATA LOCAL INFILE '/tmp/some_file.txt' INTO TABLE entries FIELDS TERMINATED BY ' | ' LINES TERMINATED BY '\n';
