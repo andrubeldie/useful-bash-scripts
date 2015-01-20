1427cb8d6cb9d4dd937cf4bf80f2696394a0e23f
Andru Beldie <andru.beldie@gmail.com>
2015-01-20 10:06:05 +0200

    Update simple-mysql-full-backup.sh

--- simple-mysql-full-backup.sh
+++ simple-mysql-full-backup.sh
@@ -3,7 +3,7 @@
 # this script requires that in console there is no need to insert username and password
 # to get this all you have to do is to set in ~/.my.cnf
 # [client]
-# pass="y_o_u_r_p_a_s_s_w_o_r_d"
+# password="y_o_u_r_p_a_s_s_w_o_r_d"
 # user=y_o_u_r_u_s_e_r_n_a_m_e
 
 #please note the quotes for password
